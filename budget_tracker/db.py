152019ec3a1bfd89d907e3f61ed391af0e170811
jacklyne <jacklyne.owuor@student.moringaschool>
2025-12-03 11:26:55 +0300

    added content

--- budget_tracker/db.py
+++ budget_tracker/db.py
@@ -1,3 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import DATABASE_URL
+
+engine = create_engine(DATABASE_URL, echo=False, future=True)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
+Base = declarative_base()
+
+def init_db():
+    # Import models so Base knows about them
+    from . import models  # noqa: F401
+    Base.metadata.create_all(bind=engine)
